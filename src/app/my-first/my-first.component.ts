7c83966657980264b605ae0ba1b16fc7854e4544
Gabriel Leal Sala de Oliveira <gabriel.sala.oliveira@outlook.com>
2022-01-10 18:14:33 -0800

    Create components with and without CLI

---
+++ src/app/my-first/my-first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-first-component',
+  template: `
+    <p>My first Component with Angular!</p>
+  `
+})
+export class MyFirstComponent {
+
+}
